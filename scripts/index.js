710f8a896083b30cc3539c48ae838d0646d2d935
Алексей Алексеев <alexseev617@gmail.com>
2024-11-07 18:08:39 +0300

    Закончил приложение expensesApp

--- scripts/index.js
+++ scripts/index.js
@@ -1,98 +1,144 @@
-const inputNode = document.querySelector('#expenses-input');
-const buttonNode = document.querySelector('#expenses-btn');
-const historyNode = document.querySelector('#expenses-history')
-const sumNode = document.querySelector('#expenses-sum');
-const limitNode = document.querySelector('#expenses-limit')
-const statusNode = document.querySelector('#expenses-status')
-
-const LIMIT = 10000;
-const CURRENCY = 'руб.'
 const STATUS_IN_LIMIT = 'все хорошо';
-const STATUS_OUT_LIMIT = 'все плохо';
-const STATUS_OUT_OF_LIMIT_CLASSNAME = 'expenses_status-red';
+const STATUS_OUT_OF_LIMIT = 'все плохо';
+const STORAGE_LABEL_LIMIT = 'limit';
+const STORAGE_LABEL_EXPENSES = 'expenses';
 
-const expenses = [];
+const inputNode = document.getElementById('expensesInput');
+const categorySelectNode = document.getElementById('categorySelect')
+const addButtonNode = document.getElementById('addButton');
+const clearButtonNode = document.getElementById('clearButton')
+const changeButtonNode = document.getElementById('limitButton')
+const limitNode = document.getElementById('limitValue')
+const totalValueNode = document.getElementById('totalValue');
+const statusNode = document.getElementById('statusText')
+const historyList = document.getElementById('historyList')
 
-init(expenses);
+let expenses = [];
 
+let LIMIT = parseInt(limitNode.innerText)
+const CURRENCY = 'руб.'
 
-buttonNode.addEventListener('click', function () {
-    // Получаем значение из поля ввода, если равно "ничему", то в массив ничего не добавляется
-    const expense = getExpenseFromUser();
-
-    if (!expense) {
+const initLimit = () => {
+    const limitFromStorage = parseInt(localStorage.getItem(STORAGE_LABEL_LIMIT))
+    if (!limitFromStorage) {
         return;
     }
-
-    trackExpense(expense);
-
-    render(expenses);
-})
-
-function init(expenses) {
-    limitNode.innerText = LIMIT;
-    statusNode.innerText = STATUS_IN_LIMIT;
-    sumNode.innerText = calculateExpenses(expenses);
+    limitNode.innerText = limitFromStorage;
+    LIMIT = parseInt(limitNode.innerText)
 }
 
-function trackExpense(expense) {
-    expenses.push(expense)
-}
+initLimit();
 
-function getExpenseFromUser() {
-    if (!inputNode.value) {
-        return null;
-    }
 
-    const expense = parseInt(inputNode.value);
 
-    clearInput();
-
-    return expense;
-}
-
-function clearInput() {
-    inputNode.value = '';
-}
-
-function calculateExpenses(expenses) {
+const getTotal = () => {
     let sum = 0;
+    //сделать тоже самое только через for
+    expenses.forEach((newExpense) => {
+        sum += newExpense.expense;
 
-    expenses.forEach(exspense => {
-        sum += exspense;
     });
-
     return sum;
 }
 
-function render(expenses) {
-    const sum = calculateExpenses(expenses);
+const renderStatus = () => {
+    const total = getTotal(expenses);
+    totalValueNode.innerText = total;
 
-    renderHistory(expenses);
-    renderSum(sum);
-    renderStatus(sum);
-}
-
-function renderHistory(expenses) {
-    let historyHTML = '';
-
-    expenses.forEach(exspense => {
-        const exspenseHTML = `<li>${exspense} ${CURRENCY}</li>`;
-        historyHTML += exspenseHTML;
-    });
-
-    historyNode.innerHTML = `<ol>${historyHTML}</ol>`
-}
-
-function renderSum(sum) {
-    sumNode.innerText = sum;
-}
-
-function renderStatus(sum) {
-    if (sum <= LIMIT) {
+    if (total <= LIMIT) {
         statusNode.innerText = STATUS_IN_LIMIT;
+        statusNode.className = `expenses_status-green`
     } else {
-        statusNode.innerText = STATUS_OUT_LIMIT;
-        statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME)
+        statusNode.innerText = `${STATUS_OUT_OF_LIMIT}(${LIMIT - total})`;
+        statusNode.className = 'expenses_status-red'
     }
 }
+
+const renderHistory = () => {
+    historyList.innerHTML = '';
+    expenses.forEach((newExpense) => {
+        const historyItem = document.createElement('li');
+        historyItem.innerText = `${newExpense.category} - ${newExpense.expense} ${CURRENCY}`;
+
+        //попробовать написать через historyItem.innerText = `<li class="rub">`
+
+        historyList.appendChild(historyItem);
+    })
+}
+
+const render = () => {
+    renderStatus();
+    renderHistory();
+}
+
+const expensesFromStorageString = localStorage.getItem(STORAGE_LABEL_EXPENSES);
+const expensesFromStorage = JSON.parse(expensesFromStorageString)
+
+if (Array.isArray(expensesFromStorage)) {
+    expenses = expensesFromStorage;
+}
+render();
+
+const getExpenseFromUser = () => parseInt(inputNode.value);
+
+const getSelectedCategory = () => categorySelectNode.value
+
+const clearInput = () => inputNode.value = '';
+
+function saveExpensesToStorage() {
+    const expensesString = JSON.stringify(expenses);
+    localStorage.setItem(STORAGE_LABEL_EXPENSES, expensesString);
+}
+
+const addButtonHandler = () => {
+    const expense = getExpenseFromUser();
+    if (!expense) {
+        alert("Выберете введите сумму")
+        return;
+    }
+
+    const category = getSelectedCategory();
+    if (category === 'категории') {
+        alert("Выберете категорию")
+        return;
+    }
+
+    const newExpense = {
+        expense: expense,
+        category: category,
+    }
+
+    console.log(newExpense);
+
+    expenses.push(newExpense);
+
+    saveExpensesToStorage();
+    render();
+    clearInput();
+
+
+}
+
+const clearButtonHandler = () => {
+    expenses = [];
+    render();
+}
+
+const changeButtonHandler = () => {
+    const newLimit = prompt('введите новый лимит');
+
+    const newLimitValue = parseInt(newLimit);
+    if (!newLimitValue) {
+        return;
+    }
+
+    limitNode.innerText = newLimitValue;
+    LIMIT = newLimitValue;
+    localStorage.setItem(STORAGE_LABEL_LIMIT, newLimitValue)
+
+    render();
+}
+
+addButtonNode.addEventListener('click', addButtonHandler);
+clearButtonNode.addEventListener('click', clearButtonHandler);
+changeButtonNode.addEventListener('click', changeButtonHandler)
